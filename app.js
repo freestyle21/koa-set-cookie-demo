a42f894580b35e78105623724b5bb34dd5b1d0df
qubaoming <qubaoming@didichuxing.com>
2016-03-21 23:04:53 +0800

    init file

--- app.js
+++ app.js
@@ -1,114 +1,43 @@
-var fs = require('fs');
+'use strict';
 
-var koa = require('koa');
-var parse = require('co-body');
-var Mustache = require('mustache');
+const koa     = require('koa')
+const http    = require('http')
+const path    = require('path')
+const router = require('koa-router')();
+const render  = require('koa-views')
+const serve   = require('koa-static')
+const app = koa()
 
-var app = koa();
-
-// logger, Ripped from koajs.com
-
-app.use(function *(next){
-  var start = new Date;
-  yield next;
-  var ms = new Date - start;
-  console.log(this.method + " " + this.url + ", " + ms + " milliseconds");
+app.use(function*(next) {
+  this.cookies.set('test', 999999);
+  return yield next;
 })
 
-// for the cookies
+// serve files from ./client
+app.use(serve(path.join(__dirname, './')))
+   .use(render(path.join(__dirname, "./"), {
+        map: {
+           html: 'ejs'
+        }
+   }))
 
-app.keys = ['totally', 'secret', 'keys'];
 
-app.use(function *(next){
-  
-  var n = parseInt(this.cookies.get('views')) + 1;
-
-  this.cookies.set('views', (n) ? n : 1);
+router.get('/v1/test/', function *(next) {
+  this.cookies = {test6666: 66666}
+  this.body = {code: 200, message: 'success'}
   return yield next;
-});
+})
 
-// Responds to a POST request. Writes to directory with given filename and msg.
 
-app.use(function *(next){
+router.get('/', function *(next) {
+  yield this.render('./index')
+})
 
-  if ('POST' !== this.method) return yield next;
-  var body = yield parse(this);
-  fs.writeFile(__dirname + '/' + body.filename, body.msg, 'utf8', function(err){
-    if (err) console.log(err);
-  });
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
 
-  yield next;
-});
 
-// this serves index.html at the base url. Uses fs.readdir to get a list of all available files.
+http.createServer(app.callback()).listen(3000)
+console.log('listen on 3000.')
 
-app.use(function *(next){
-  if ("/" !== this.path) return yield next;
-  var template = yield readFile('index.html');
-  var allFiles = yield readdir();
-  this.type = 'html';
-  this.status = 200;
-  this.body = Mustache.render(template, {'files': allFiles, 'views': parseInt(this.cookies.get('views')) + 1 || 1});
-});
-
-// serves contents of a given file (matching the url path).
-// if the url path is instead a directory, it lets the user know.
-// if the url path has no match in the directory, it yields downstream
-
-app.use(function *(next){
-
-  try {
-    var fileStat = yield stat(this.path);    
-  } catch (e) {
-    return yield next;
-  }
-  
-  if (fileStat.isFile()) {
-    this.type = 'html';
-    this.status = 200;
-    this.body = yield readFile(__dirname + this.path);
-  } else {
-    this.type = 'html';
-    this.status = 200;
-    this.body = "<h1>That there is a directory</h1>"
-  }
-});
-
-// 404 response
-
-app.use(function *(){
-  this.type = 'html';
-  this.status = 404;
-  this.body = "<h1>I have nothing like that</h1>"
-});
-
-app.listen(3000);
-
-// helper Promisifications
-
-function readFile(fileName){
-  return new Promise(function(resolve, reject){
-    fs.readFile(fileName, 'utf8', function(err, data){
-      if (err) return reject(err);
-      resolve(data);
-    });
-  });
-}
-
-function readdir(){
-  return new Promise(function(resolve, reject){
-    fs.readdir('.', function(err, files){
-      if (err) return reject(err);
-      resolve(files);
-    });
-  });
-}
-
-function stat(file){
-  return new Promise(function(resolve, reject){
-    fs.stat(__dirname + file, function(err, data){
-      if (err) return reject(err);
-      resolve(data);
-    });
-  });
-}
